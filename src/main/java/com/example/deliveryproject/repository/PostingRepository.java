42f5905984ccf29dbd5f5f39e1c1d20fec6f3710
vbestaeva <vbestaeva@ozon.ru>
2023-05-09 20:43:19 +0300

    Убрала комментарии

--- src/main/java/com/example/deliveryproject/repository/PostingRepository.java
+++ src/main/java/com/example/deliveryproject/repository/PostingRepository.java
@@ -8,11 +8,6 @@ import org.springframework.data.repository.query.Param;
 public interface PostingRepository extends JpaRepository<Posting, Long> {
     Posting findByPostingNumber(String postingNumber);
 
-//    Long findIdByPostingNumber(String postingNumber);
-
-//    @Query(nativeQuery = true, value = "SELECT * FROM SLSNotification s WHERE s.userId = :userId ORDER BY snumber DESC LIMIT 20")
-//    List<SLSNotification> getUserIdforManage(@Param("userId") String userId);
-
     @Query(nativeQuery = true, value = "SELECT max(id) FROM postings WHERE posting_number = :postingNumber LIMIT 1")
     Long findIdByPostingNumber(@Param("postingNumber") String postingNumber);
 
